b5da17cff6b4e7035e9d913d69a4d46d7b84a6bf
Sergey Mokeyev <sergey.mokeyev@gmail.com>
2019-06-25 15:50:44 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=['tests', 'examples']),
     classifiers=[
         'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: GPL3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: POSIX',
     ],
     install_requires=requirements
